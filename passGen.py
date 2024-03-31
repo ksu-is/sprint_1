53a8f4dc413b9595ee7c1757dac40da40358e53c
alsaffar501 <156117876+alsaffar501@users.noreply.github.com>
2024-03-31 15:46:21 -0400

    Update passGen.py
    I have finished the code after fixing the issues, and I debugged to make sure the code works 100%.

--- passGen.py
+++ passGen.py
@@ -9,3 +9,19 @@ def pWord():
     special_count = int(input("How many special characters? "))
     
     total_count = num_count + char_count + special_count
+    
+    if total_count > length:
+        print("you have exceeded password length. Please try again.")
+        return
+    
+    num = ''.join(random.choices(string.digits, k=num_count))
+    chars = ''.join(random.choices(string.ascii_letters, k=char_count))
+    special = ''.join(random.choices(string.punctuation, k=special_count))
+    
+    the_key = num + chars + special
+    
+    pass_list = list(the_key)
+    random.shuffle(pass_list)
+    the_key = ''.join(pass_list)
+    
+    print("Your customized randomized password is:", the_key)
